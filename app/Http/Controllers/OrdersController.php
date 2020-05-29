31cfc19d7822dafa5e4a9ddcd87b5bbf6d068d38
Adesanya Ayokunle <adesanyaayokunle@gmail.com>
2020-05-29 13:04:48 +0100

    fix order email

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -90,8 +90,7 @@ class OrdersController extends Controller
                 return $this->sendError("You can't purchase more than 200 fractions of the same property", null, 409);
             };
 
-            Cache::forget('order:' . $user->id);
-            Cache::forget('invoice:' . $user->id);
+            Cache::flush();
 
             //validate yield period
             if (!in_array($request->input('yield_period'), $property->holding_period)) {
@@ -117,7 +116,6 @@ class OrdersController extends Controller
             $invoice->update();
 
             //Send email
-            $user =  Auth::user();
             $props = $order->property()->get();
 
             $expected_returns = $this->expectedCash($props[0]->net_rental_yield[$order->yield_period], $order->price);
