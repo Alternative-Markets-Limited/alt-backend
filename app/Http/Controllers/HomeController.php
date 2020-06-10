b619ab186112441f2c07162c7f2df551790ac33d
Adesanya Ayokunle <adesanyaayokunle@gmail.com>
2020-06-10 12:28:32 +0100

    added cache flush (#35)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        Cache::flush();
         return response()->json(["name" => "Alt", "version" => "1.0.0", "description" => "Welcome to Alt api"]);
     }
 }
